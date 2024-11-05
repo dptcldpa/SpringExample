27b5a374a28c56d0bc5ea153a97acee37ac1e71c
조해민 <dptcldpa@gmail.com>
2024-11-05 18:48:09 +0900

    [조해민]  - Mybatis 예제 2 INSERT 쿼리  - gradle에 p6spy 프레임워크 추가

--- src/main/java/com/dptcldpa/spring/ex/mybatis/repository/ReviewRepository.java
+++ src/main/java/com/dptcldpa/spring/ex/mybatis/repository/ReviewRepository.java
@@ -7,7 +7,25 @@ import com.dptcldpa.spring.ex.mybatis.domain.Review;
 
 @Mapper
 public interface ReviewRepository {
+	
+	// SELECT 쿼리
 
 	// 전달된 id와 일치하는 리뷰 행 조회
 	public Review selectReview(@Param("id") int id);
+
+	
+	// INSERT 쿼리
+	
+	// 전달받은 리뷰 정보를 기반으로 insert한다.
+	public int insertReview(
+			@Param("storeId") int storeId
+			, @Param("menu") String menu
+			, @Param("userName") String userName
+			, @Param("point") double point
+			, @Param("review") String review);
+	
+	
+	// 전달받은 Review 객체로 insert한다.
+	public int insertReviewByObject(Review review);
+	
 }
--- src/main/java/com/dptcldpa/spring/ex/mybatis/service/ReviewService.java
+++ src/main/java/com/dptcldpa/spring/ex/mybatis/service/ReviewService.java
@@ -11,6 +11,8 @@ public class ReviewService {
 	
 	@Autowired
 	private ReviewRepository reviewRepository;
+	
+	// SELECT 쿼리
 
 	// 전달받은 id와 대응되는 리뷰정보 얻어오기
 	public Review getReview(int id) {
@@ -19,6 +21,34 @@ public class ReviewService {
 		
 		return review;
 		
+	}
+	
+	
+	// INSERT 쿼리
+	
+	// 전달받은 리뷰 정보를 기반으로 리뷰 저장
+	public int addReview(
+			int storeId
+			, String menu
+			, String userName
+			, double point
+			, String review) {
+		
+		// 전달받은 리뷰내용으로 new_review 테이블에 insert
+		int count = reviewRepository.insertReview(storeId, menu, userName, point, review);
+		
+		return count;
+		
+	}
+
+	
+	// 전달받은 Review 객체를 통해 저장
+	public int addReviewByObject(Review review) {
+		
+		// 전달받은 Review 객체로 new_review에 insert
+		int count = reviewRepository.insertReviewByObject(review);
+		
+		return count;
 	}
 	
 }
