bfb6a5db5d0c82134fe2eb996910b2544aee5886
조해민 <dptcldpa@gmail.com>
2024-11-06 18:48:07 +0900

    [조해민]  - MVC 예제 1

---
+++ src/main/java/com/dptcldpa/spring/ex/mvc/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.dptcldpa.spring.ex.mvc.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.dptcldpa.spring.ex.mvc.domain.User;
+
+@Mapper
+public interface UserRepository {
+
+	public int insertUser(
+			@Param("name") String name
+			, @Param("birthday") String birthday
+			, @Param("email") String email
+			, @Param("introduce") String introduce);
+	
+	public User selectLastUser();
+	
+}
